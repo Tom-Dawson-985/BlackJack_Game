f64f6c7c0b4dc7e164123636feb3501ec084ad0d
Harryd990 <harryjduff@icloud.com>
2025-01-30 12:10:09 +0000

    added a get cards in the card file so u can use the get cards function to get a card saved as Card

--- BlackJack Game/Cards.cs
+++ BlackJack Game/Cards.cs
@@ -5,34 +5,37 @@ namespace BlackJack_Game;
 
 public class Cards
 {
-<<<<<<< HEAD
+
     public int[] _cardSet =
     {
         1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10,
         10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10
     };
 
-
-    private int[] _RoundCards = new int[_cardSet.Length];
-=======
-    // the deck that is used to restore the deck after each round
-    string[] _freshDeck  = new string[] {"1","2","3","4","5","6","7","8","9","10","J","Q","K","A"};
-    
-    // the deck that is used when playing
-    public HashSet<string> DeckOfCards = new HashSet<string>() {"1","2","3","4","5","6","7","8","9","10","J","Q","K","A"};
-
-    public void NewGame() // triggered at the end of a round, adds all cards back to the deck
+    public void GetCard()
     {
-        foreach (string card in DeckOfCards) // empty the set
-        {
-            DeckOfCards.Remove(card);
-        }
+        Random rnd = new Random();
+        int CardNumber = rnd.Next(1, 101);
+        
+        int Card = _cardSet[CardNumber];
 
-        foreach (string card in _freshDeck) // restore the cards
-        {
-            DeckOfCards.Add(card);
-        }
+        
+
     }
->>>>>>> 9286a215b4121a1db5c934d74a2dc67327204165
+    
+    
+    //public void NewGame() // triggered at the end of a round, adds all cards back to the deck
+    //{
+      //  foreach (string card in DeckOfCards) // empty the set
+        //{
+          //  DeckOfCards.Remove(card);
+        //}
+
+       // foreach (string card in _freshDeck) // restore the cards
+        //{
+          //  DeckOfCards.Add(card);
+        //}
+    //}
 }
+
 
