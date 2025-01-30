8a9d0fb4967e8ccfdfe7e5edc8115993ab6ab915
Harryd990 <harryjduff@icloud.com>
2025-01-30 09:10:16 +0000

    Update Cards.cs

--- BlackJack Game/Cards.cs
+++ BlackJack Game/Cards.cs
@@ -3,11 +3,13 @@ namespace BlackJack_Game;
 
 internal class Cards
 {
-    private int[] _cardSet =
+    public int[] _cardSet =
     {
         1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10,
         10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10
     };
-    
-    // fuck harry duff
+
+
+    private int[] _RoundCards = new int[_cardSet.Length];
 }
+
